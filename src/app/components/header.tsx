a29b5d65badc6232bb626b00aace776c972c2e08
Korbin101lee <khillan5223@gmail.com>
2025-02-09 09:26:03 -0800

    fixed tailwind.css not working correctly

---
+++ src/app/components/header.tsx
@@ -0,0 +1,10 @@
+import "../globals.css"; // Ensure this path is correct relative to RootLayout
+
+export default function Header() {
+    return (
+      <header className="bg-red-500 p-4">
+        <h1 className="text-3xl font-bold text-white">Header is Working!</h1>
+      </header>
+    );
+  }
+  
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+import Header from "./components/header";
+
 export default function Home() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-  )
+    <div>
+      <Header />
+      <h1 className="bg-red-500 text-3xl font-bold underline">Home Page</h1>
+    </div>
+  );
 }
