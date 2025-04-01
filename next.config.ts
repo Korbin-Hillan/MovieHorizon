5fd14e993b1bfdfc64563360580a83b99a510cbf
Korbin101lee <khillan5223@gmail.com>
2025-03-31 21:25:31 -0700

    removed unused includes causing failed actions

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: 'export', // Add this line to enable static export
   images: {
+    unoptimized: true, // Required for static export
     remotePatterns: [
       {
-        protocol: 'https', // Allows all HTTPS images
+        protocol: 'https',
         hostname: '**', // Allows any domain
       },
     ],
