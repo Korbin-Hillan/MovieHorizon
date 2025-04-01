3902f2b8c4ab1d1340a06553fc17c5a77dbbc722
Korbin101lee <khillan5223@gmail.com>
2025-03-31 21:27:59 -0700

    updated the yml file

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export', // Add this line to enable static export
   images: {
-    unoptimized: true, // Required for static export
     remotePatterns: [
       {
-        protocol: 'https',
+        protocol: 'https', // Allows all HTTPS images
         hostname: '**', // Allows any domain
       },
     ],
